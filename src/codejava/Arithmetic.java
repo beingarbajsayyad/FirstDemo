2c92b811e6a98b8042adf4345b6e6189b09d0a66
arbaj <arbaj@Beingarbajsayyad>
2023-05-12 09:08:47 +0530

    Arbaj Commiting code for arithmatic

--- src/codejava/Arithmetic.java
+++ src/codejava/Arithmetic.java
@@ -5,6 +5,7 @@ public class Arithmetic {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Inside arithmatic");
 
    int a= 10;
    int b= 20;
